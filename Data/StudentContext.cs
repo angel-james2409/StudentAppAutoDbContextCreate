92edcb68d7d74df6e40cfc92c22b1000fa46ea72
Angel <ngeljames2409@gmail.com>
2021-01-06 20:53:31 +0530

    final commit model first approach

---
+++ Data/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentAppAutoDbContextCreate;
+
+namespace StudentAppAutoDbContextCreate.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext (DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentAppAutoDbContextCreate.Student> Student { get; set; }
+    }
+}
